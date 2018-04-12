222c7d540eb3f45c87b74259b3dedc356a245ed3
Brendan Asselstine <mail.asselstine@gmail.com>
2018-04-12 15:12:36 -0700

    Fixed broken JS include

--- app/javascripts/components/application/layout/site-header/index.jsx
+++ app/javascripts/components/application/layout/site-header/index.jsx
@@ -7,18 +7,18 @@ require('./site-header.css')
 export default class extends Component {
   render () {
     return (
-      <nav id="navbar" class="navbar">
-        <div id="specialShadow" class="bd-special-shadow">
+      <nav id="navbar" className="navbar">
+        <div id="specialShadow" className="bd-special-shadow">
         </div>
 
-        <div class="container">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="https://ivyblockchainawards.com">
+        <div className="container">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://ivyblockchainawards.com">
               <img src="/images/logos/ivy--symbol.png" />
               <img src="/images/logos/ivy--logo.png" />
             </a>
 
-            <div id="navbarBurger" class="navbar-burger burger" data-target="navMenuDocumentation">
+            <div id="navbarBurger" className="navbar-burger burger" data-target="navMenuDocumentation">
               <span></span>
               <span></span>
               <span></span>
@@ -26,7 +26,6 @@ export default class extends Component {
           </div>
         </div>
       </nav>
-
     )
   }
 }
