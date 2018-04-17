867de4b37c171aa495101a9a77ac7602c26a99b3
Brendan Asselstine <mail.asselstine@gmail.com>
2018-04-17 14:13:56 -0700

    QR code now works on desktop and mobile

--- app/javascripts/components/application/customize-award/qr-reader/index.jsx
+++ app/javascripts/components/application/customize-award/qr-reader/index.jsx
@@ -4,22 +4,40 @@ import React, {
   Component
 } from 'react'
 import PropTypes from 'prop-types'
+import QrReaderWebrtc from './qr-reader-webrtc'
 import QrReaderImage from './qr-reader-image'
 
 class QrReader extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      useMediaDevices: false
+    }
+  }
 
   componentDidMount () {
-    window.navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-      .then((stream) => {
-        alert('Found stream', stream)
-      })
-      .catch((error) => {
-        alert(error)
-      })
+    if (!window.navigator || !window.navigator.mediaDevices) {
+      this.setState({ useMediaDevices: false })
+    }
+    else {
+      window.navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+        .then((stream) => {
+          this.setState({ useMediaDevices: true })
+        })
+        .catch((error) => {
+          this.setState({ useMediaDevices: false })
+        })
+    }
   }
 
   render () {
-    return <QrReaderImage onAddress={this.props.onAddress} />
+    var component
+    if (this.state.useMediaDevices) {
+      component = <QrReaderWebrtc onAddress={this.props.onAddress} />
+    } else {
+      component = <QrReaderImage onAddress={this.props.onAddress} />
+    }
+    return component
   }
 }
 
