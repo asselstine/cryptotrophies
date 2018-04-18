ad0f5766771c3b024ad25c36f2cb72c1e7ae6292
Brendan Asselstine <mail.asselstine@gmail.com>
2018-04-18 16:33:41 -0700

    Cleaned up single award a little

--- app/javascripts/components/application/award/index.jsx
+++ app/javascripts/components/application/award/index.jsx
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom'
 import { Address } from '@/components/address'
 import awardUrl from '@/services/award-url'
 import CryptoTrophies from '@/contracts/cryptotrophies-factory'
+import getAward from '@/services/get-award'
+
+require('./style')
 
 export default class extends Component {
   constructor (props) {
@@ -21,25 +24,12 @@ export default class extends Component {
 
   componentDidMount () {
     var awardId = this.awardId()
-    CryptoTrophies().then((instance) => {
-      instance.getAwardType(awardId).then((response) => {
-        var awardType = parseInt(response.toString())
-        this.setState({type: awardType})
-      })
-
-      instance.getAwardTitle(awardId).then((response) => {
-        var awardTitle = response.toString()
-        this.setState({title: awardTitle})
-      })
-
-      instance.getAwardInscription(awardId).then((response) => {
-        var awardInscription = response.toString()
-        this.setState({inscription: awardInscription})
-      })
-
-      instance.getAwardRecipient(awardId).then((response) => {
-        var awardRecipient = response.toString()
-        this.setState({recipient: awardRecipient})
+    getAward(awardId).then((values) => {
+      this.setState({
+        type: values[0],
+        title: values[1],
+        inscription: values[2],
+        recipient: values[3]
       })
     })
   }
@@ -48,13 +38,14 @@ export default class extends Component {
     var content
     if (this.state.type !== null) {
       content = (
-        <div className="columns">
-          <div className="column is-three-quarter-desktop">
-            <div className="media">
-              <div className="media-content">
-                <p className="title is-4">{this.state.title}</p>
-              </div>
-            </div>
+
+        <div className="columns is-centered">
+          <div className='column is-half-desktop has-text-centered'>
+            <figure className="award__image">
+              <img src={awardUrl(this.state.type)} />
+            </figure>
+
+            <p className="title is-4">{this.state.title}</p>
 
             <div className="content">
               <p>
@@ -64,13 +55,6 @@ export default class extends Component {
                 Recipient: <small><Address address={this.state.recipient} /></small>
               </p>
             </div>
-          </div>
-          <div className='column is-one-quarter-desktop'>
-            <figure className="image">
-              <Link to={`/awards/${this.awardId()}`}>
-                <img src={awardUrl(this.state.type)} />
-              </Link>
-            </figure>
           </div>
         </div>
       )
