389eef85dcda0882c3cdc76771b0722d8b0b4057
Charles Bergeron <chuck.bergeron@gmail.com>
2018-04-18 18:41:54 -0700

    * added mobile support

--- app/javascripts/components/application/layout/site-header/index.jsx
+++ app/javascripts/components/application/layout/site-header/index.jsx
@@ -7,6 +7,31 @@ import '@/../stylesheets/app';
 import './site-header.css';
 
 export default class extends Component {
+
+  componentDidMount(){
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+
+          // Get the target from the "data-target" attribute
+          var target = $el.dataset.target;
+          var $target = document.getElementById(target);
+
+          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+          $el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
+  }
+
   render () {
     return (
       <nav id="navbar" className="navbar">
@@ -20,9 +45,15 @@ export default class extends Component {
                 <img src="/images/logos/ivy--logo-and-wordmark-small.png" />
               </div>
             </Link>
+
+            <a role="button" className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
 
-          <div className="navbar-menu">
+          <div className="navbar-menu" id="navMenu">
             <div className="navbar-end">
               <Link to='/awards/received' className='navbar-item'>
                 <span>My Awards</span>
