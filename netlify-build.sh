f2e38cb772afc90ea2d0dab769a2203c96a019e7
Brendan Asselstine <mail.asselstine@gmail.com>
2018-04-28 15:14:01 -0700

    Removed testing from netlify build

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 
-./ganache.sh &
-npm test && \
-truffle test && kill %- && \
 ./lambda-build.sh && \
 truffle compile && \
-# truffle-migrate-off-chain --network ropsten && \
+truffle-migrate-off-chain --network ropsten && \
 npm run build
